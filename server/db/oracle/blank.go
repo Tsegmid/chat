d49c38887afb332e861ee7f5f70ac75497dafb79
Tsegmid Dolgorsuren <se11d001@csms.edu.mn>
2020-08-25 14:51:08 +0800

    Add files via upload

---
+++ server/db/oracle/blank.go
@@ -0,0 +1,7 @@
+// +build !oracle
+
+// This file is needed for conditional compilation. It's used when
+// the build tag 'oracle' is not defined. Otherwise the adapter.go
+// is compiled.
+
+package oracle
